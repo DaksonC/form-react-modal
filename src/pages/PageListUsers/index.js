4705292a09c7547616f1f6f74bf7a765436e6c2d
Dakson Cruz <daksonc@gmail.com>
2022-07-22 14:31:39 -0300

    feat: cria layout da página List Users

--- src/pages/PageListUsers/index.js
+++ src/pages/PageListUsers/index.js
@@ -1,14 +1,16 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import './style.css'
 
 export function PageListUsers () {
+    const [ users, setUsers ] = useState([])
 
     // Get the users
     useEffect(() => {
         axios.get('http://localhost:3333/users')
         .then((response) => {
-            console.log(response.data)
+            setUsers(response.data)
         })
         .catch(() => {
             console.log('Deu ruim!')
@@ -16,9 +18,34 @@ export function PageListUsers () {
     }, [])
 
     return(
-        <>
-            <h1>Page List Users</h1>
-            <Link to='/'>Home</Link>
-        </>
+        <div className='container'>
+            <div className='header'>
+                <h1>Page List Users</h1>
+                <Link to='/'>Home</Link>
+            </div>
+            <div >
+                {users.map((users, key) => {
+                    return(
+                        <div className='card' key={key}>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <li>
+                                            <h1>{users.nome}</h1>
+                                        </li>
+                                        <li>
+                                            <p>E-mail: {users.email}</p>
+                                        </li>
+                                        <li>
+                                            <p>Linguagem: {users.language}</p>
+                                        </li>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
